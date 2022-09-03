8a02948ae894777c1d38812492e2815608d56c68
dupen0618 <dupen0618@163.com>
2022-09-03 10:54:26 +0800

    update_0903

--- src/api/historicalData.js
+++ src/api/historicalData.js
@@ -1,10 +1,17 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 export function fetchData(query) {
   return request({
-    url: '/api/HistoricalData/pageList',
-    method: 'post',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    params: query
-  })
+    url: "/api/HistoricalData/pageList",
+    method: "post",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    params: query,
+  });
+}
+
+export function reloadLines() {
+  return request({
+    url: "/api/HistoricalData/reloadLines",
+    method: "get",
+  });
 }
